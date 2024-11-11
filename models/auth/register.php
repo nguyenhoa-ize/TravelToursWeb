6368f10ff27c5eb8d7f3d9e38cd392040352a872
lam <2251120222@ut.edu.vn>
2024-11-11 22:38:43 +0700

    Login function

--- models/auth/register.php
+++ models/auth/register.php
@@ -11,11 +11,11 @@
         $errors =[];// Chứa các lỗi
 
         // Validate fullname
-        if (empty($filterAll['username'])) {
-            $errors['username']['required'] = 'Cần phải nhập họ tên';
+        if (empty($filterAll['fullname'])) {
+            $errors['fullname']['required'] = 'Cần phải nhập họ tên';
         } else {
-            if (strlen($filterAll['username']) < 5) {
-                $errors['username']['min'] = 'Họ tên phải có ít nhất 5 ký tự.';
+            if (strlen($filterAll['fullname']) < 5) {
+                $errors['fullname']['min'] = 'Họ tên phải có ít nhất 5 ký tự.';
             }
         }
         // validate email: bắt buộc phải nhập, đúng định dạng, kiểm tra email đã tồn tại hay chưa
@@ -31,7 +31,7 @@
         
         // Validate số điện thoại: bắt buộc phải nhập, số có đúng định dạng không
         if (empty($filterAll['phone'])) {
-            $errors['phone']['required'] = 'Số điện thoại bắt buộc phải nhập.';
+            $errors['phone']['required'] = 'Bạn chưa nhập số điện thoại.';
         } else {
             if(!isPhone($filterAll['phone'])){
                 $errors['phone']['isPhone'] = 'Số điện thoại không hợp lệ.';
@@ -41,8 +41,8 @@
         if (empty($filterAll['password'])) {
             $errors['password']['required'] = 'Mật khẩu bắt buộc phải nhập.';
         } else {
-            if(strlen($filterAll['password']) < 8){
-                $errors['password']['min'] = 'Mật khẩu phải lớn hơn hoặc bằng 8.';
+            if(strlen($filterAll['password']) < 6){
+                $errors['password']['min'] = 'Mật khẩu phải lớn hơn hoặc bằng 6.';
             }
         }
         // Validate pasword_confirm: bắt buộc phải nhập, giống password
@@ -56,7 +56,7 @@
     }
     if(empty($errors)){
         $dataInsert = [
-            'username' => $filterAll['username'],
+            'fullname' => $filterAll['fullname'],
             'email' => $filterAll['email'],
             'phone' => $filterAll['phone'],
             'password' => password_hash($filterAll['password'],PASSWORD_DEFAULT),
@@ -75,10 +75,17 @@
             setFlashData('smg_type','danger');
             setFlashData('errors',$errors);
             setFlashData('old',$filterAll);
+            redirect('register.php');
             
         }
+    
+    } else {
+    // Nếu có lỗi, lưu lại thông tin đã nhập
+    setFlashData('errors', $errors);
+    setFlashData('old', $filterAll);
     }
 }
+
 $smg = getFlashData('smg');
 $smg_type = getFlashData('smg_type');
 $errors = getFlashData('errors');
@@ -142,34 +149,40 @@ $old = getFlashData('old');
 </head>
 <body>
 <div class="register-container">
+    <?php 
+            if(!empty($smg)){
+               getSmg($smg,$smg_type);
+            }
+           
+    ?>
     <form action="" method="post">
     <h2>Đăng Ký Tài Khoản</h2>
-    <input type="text" name="username" id="username" placeholder="Họ và tên" value="<?php 
-    echo old('username',$old);
+    <?php 
+        echo form_error('fullname','<span class="error">','</span>',$errors );
+    ?>
+    <input type="text" name="fullname" id="fullname" placeholder="Họ và tên" value="<?php 
+    echo old('fullname',$old);
     ?>">
     <?php 
-        echo form_error('username','<span class="error">','</span>',$errors );
+        echo form_error('email','<span class="error">','</span>',$errors );
     ?>
     <input type="email" name="email" id="email" placeholder="Email"value="<?php 
     echo old('email',$old);
     ?>">
     <?php 
-        echo form_error('email','<span class="error">','</span>',$errors );
+        echo form_error('phone','<span class="error">','</span>',$errors );
     ?>
     <input type="number" name="phone" id="phone" placeholder="Số điện thoại"value="<?php 
     echo old('phone',$old);
     ?>">
     <?php 
-        echo form_error('phone','<span class="error">','</span>',$errors );
+        echo form_error('password','<span class="error">','</span>',$errors );
     ?>
     <input type="password" name="password" id="password" placeholder="Mật khẩu">
     <?php 
-        echo form_error('password','<span class="error">','</span>',$errors );
-    ?>
-    <input type="password" name="confirm-password" id="confirm-password" placeholder="Xác nhận mật khẩu">
-    <?php 
         echo form_error('confirm-password','<span class="error">','</span>',$errors );
     ?>
+    <input type="password" name="confirm-password" id="confirm-password" placeholder="Xác nhận mật khẩu">
     <button type="submit">Đăng ký</button> 
     <a href="login.php">Đã có tài khoản?</a>
     </form>
