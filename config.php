f804008bb8a9c051ef1359c3a95218d03b368443
Nguyen Van Hoa <126589828+nguyenhoa-ize@users.noreply.github.com>
2024-11-18 09:29:15 +0700

    update connect

--- config.php
+++ config.php
@@ -7,4 +7,9 @@ define('DB_SERVER', 'localhost:3307');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'traveltoursweb');
+
+const _HOST = 'localhost:3307';
+const _DB = 'traveltoursweb';
+const _USER = 'root';
+const _PASS = '';
 ?>
